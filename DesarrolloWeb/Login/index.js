07ec07d86551aef902bbca2b0547ffa67daf56fc
Arturo Landa Espinosa <61170842+artlandae@users.noreply.github.com>
2024-03-20 18:07:49 -0600

    Add files via upload
    Bosquejo html-css proyecto terminal

---
+++ DesarrolloWeb/Login/index.js
@@ -0,0 +1,22 @@
+
+// var file = document.getElementById('correoElectronico').files[0];
+// console.log(correoElectronico)
+
+
+// // Get the file from the input element
+// var file = document.getElementById('correoElectronico').files[0];
+
+// // Create a new FileReader object
+// var reader = new FileReader();
+
+// // Set the callback function for when the file is loaded
+// reader.onload = function(event) {
+//   // Get the contents of the file
+//   var contents = event.target.result;
+
+//   // Do something with the contents, such as display them on the page
+//   document.body.innerHTML = contents;
+// };
+
+// // Read the file as text
+// reader.readAsText(file);
